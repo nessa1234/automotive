9866d46ba85339f07e48878633b7fa137a447ff9
nessa1234 <44015100+nessa1234@users.noreply.github.com>
2019-04-27 10:14:36 +0530

    Update wsgi.py

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'myproject'))
+sys.path.insert(0, root_path)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
