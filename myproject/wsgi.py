e7d632c79425e3eaf21199543bec41b0fcc53f90
nessa1234 <nessakallikkad96@gmail.com>
2019-04-27 12:46:47 +0530

    added __init__.py file

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 root_path = os.path.abspath(os.path.split(__file__)[0])
 sys.path.insert(0, os.path.join(root_path, 'myproject'))
